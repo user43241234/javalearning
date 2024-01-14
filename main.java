3d8d511f8b5300fbb6bf2a17b8f7b85dd3e2ae65
user43241234 <156609605+user43241234@users.noreply.github.com>
2024-01-14 12:18:58 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 class main {
   public static void main(String[] args) {
     System.out.println("hello");
-    String name = "Benjamin",
-    System.out.println(name);
+    System.out.println("welcome to name verification");
+    try (Scanner namever = new Scanner(System.in)) {
+      System.out.println("what is your name? >");
+      String iptname = namever.nextLine();
+      if (iptname == "ben") {
+        System.out.println("name verified. welcome, master benjamin");
+      } else {
+        System.out.println("wrong you fucking dumass");
+      }
+    }
+
+    
   }
 }
